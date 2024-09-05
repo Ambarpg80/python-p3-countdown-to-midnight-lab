e25efd9f353297887d2690c18015a5ed75d64bef
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2024-09-05 10:42:31 -0400

    added solve to solution branch

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,16 @@
-# your code goes here!
+def countdown(num):
+    i = 0
+    while i < num:
+        print(f"{num} SECOND(S)!")
+        num -= 1
+    else:
+        print("HAPPY NEW YEAR!")
+       
+   
+def countdown_with_sleep(num):
+    i = 0
+    while i < num:
+        print(f"{num} SECOND(S)!")
+        num -= 1
+    else:
+        print("HAPPY NEW YEAR!")
